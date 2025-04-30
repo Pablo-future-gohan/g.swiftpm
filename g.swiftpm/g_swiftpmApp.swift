717f02e3293e08c82cb8214864d8c7a7cccd0074
Daniel Popovich <pop512413@d204.lths.net>
2025-04-30 12:17:32 -0500

    Initial Commit

---
+++ g.swiftpm/g_swiftpmApp.swift
@@ -0,0 +1,17 @@
+//
+//  g_swiftpmApp.swift
+//  g.swiftpm
+//
+//  Created by Popovich, Daniel (512413) on 4/30/25.
+//
+
+import SwiftUI
+
+@main
+struct g_swiftpmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
